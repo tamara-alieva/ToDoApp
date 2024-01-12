931b8d690569e8fc3e9d7b31c6e3131a368b98be
Tamara <tamaraavgust14@gmail.com>
2024-01-13 01:58:33 +0700

    Добавление функции onCreate

--- app/src/main/java/com/example/todoapp/KoinApp.kt
+++ app/src/main/java/com/example/todoapp/KoinApp.kt
@@ -1,10 +1,22 @@
 package com.example.todoapp
 
 import android.app.Application
+import androidx.room.Room
+import com.example.todoapp.database.TodoDatabase
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class KoinApp:Application() {
     override fun onCreate() {
         super.onCreate()
-
+        startKoin{
+            modules(module {
+                single {
+                    Room
+                        .databaseBuilder(this@KoinApp, TodoDatabase::class.java, "db")
+                        .build()
+                }
+            })
+        }
     }
 }
