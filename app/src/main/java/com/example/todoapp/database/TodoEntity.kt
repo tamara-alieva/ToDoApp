44d9fe94e7a01b7e431477d018b2506a6706e2cc
Tamara <tamaraavgust14@gmail.com>
2024-01-13 01:28:45 +0700

    Добавление объектов

---
+++ app/src/main/java/com/example/todoapp/database/TodoEntity.kt
@@ -0,0 +1,21 @@
+package com.example.todoapp.database
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Date
+@Entity("todos")
+data class TodoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int = 0,
+    @ColumnInfo("title")
+    val title:String,
+    @ColumnInfo("sub_title")
+    val subTitle:String,
+    @ColumnInfo("done")
+    val done:Boolean = false,
+    @ColumnInfo("added")
+    val added:Long = System.currentTimeMillis()
+)
+
+val TodoEntity.addDate:String get() = SimpleDateFormat("dd.mm.yyyy").format(Date(added))
