4cece864349b9c6ea2bc524118dc8e154987a1fa
Tamara <tamaraavgust14@gmail.com>
2024-01-13 15:07:15 +0700

    Создание объектов (подготовка данных для пользовательского интерфейса)

---
+++ app/src/main/java/com/example/todoapp/screens/HomeViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.todoapp.screens
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.database.TodoEntity
+import com.example.todoapp.repositories.TodoRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class HomeViewModel:ViewModel(), KoinComponent {
+    private val repo:TodoRepo by inject()
+    private val _todos:MutableStateFlow<List<TodoEntity>> = MutableStateFlow(emptyList())
+    val todos = _todos.asStateFlow()
+
+    init {
+        getTodos()
+    }
+
+    private fun getTodos() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getTodos().collect{data->
+                _todos.update { data }
+            }
+        }
+    }
+
+    fun updateTodo(todo:TodoEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.updateTodo(todo)
+        }
+    }
+
+    fun daleteTodo(todo:TodoEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.deleteTodo(todo)
+        }
+    }
+
+    fun addTodo(todo:TodoEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.addTodo(todo)
+        }
+    }
+
+
+}
