329b77fef7afc10ecb73b15cb7684eb4fb702830
Tamara <tamaraavgust14@gmail.com>
2024-01-13 19:52:28 +0700

    Добавление шрифта

--- app/src/main/java/com/example/todoapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/Type.kt
@@ -2,14 +2,20 @@ package com.example.todoapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.todoapp.R
+
+val ubuntuFont = FontFamily(listOf(
+    Font(R.font.ubuntu)
+))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ubuntuFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
