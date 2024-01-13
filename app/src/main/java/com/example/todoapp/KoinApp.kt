6c0c012fc9d749163248e4c5cdd4ebf38e5887f9
Tamara <tamaraavgust14@gmail.com>
2024-01-13 14:49:37 +0700

    Связь TodoRepoImpl и TodoRepo

--- app/src/main/java/com/example/todoapp/KoinApp.kt
+++ app/src/main/java/com/example/todoapp/KoinApp.kt
@@ -3,7 +3,10 @@ package com.example.todoapp
 import android.app.Application
 import androidx.room.Room
 import com.example.todoapp.database.TodoDatabase
+import com.example.todoapp.repositories.TodoRepo
+import com.example.todoapp.repositories.TodoRepoImpl
 import org.koin.core.context.startKoin
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 class KoinApp:Application() {
@@ -16,6 +19,9 @@ class KoinApp:Application() {
                         .databaseBuilder(this@KoinApp, TodoDatabase::class.java, "db")
                         .build()
                 }
+                single {
+                    TodoRepoImpl(database = get())
+                } bind TodoRepo::class
             })
         }
     }
